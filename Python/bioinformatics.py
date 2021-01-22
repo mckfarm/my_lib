fa4a81e5be833ac96fe58940474420e3fa27a60c
mckfarm <51832180+mckfarm@users.noreply.github.com>
2021-01-22 09:29:25 -0600

    qiime manifest
    qiime manifest function

--- Python/bioinformatics.py
+++ Python/bioinformatics.py
@@ -166,3 +166,42 @@ def prokka_search(directory, search_term):
     result_df["bin"] = match_list
 
     return result_df
+
+def qiime_manifest(directory, expression, output):
+    '''
+    generate a qiime manifest file from a directory of 16s data
+    inputs:
+        directory = where the 16s files are located, should be in fastq.gz format
+        expression = regex expression of unique sample ids
+        output = name of manifest file
+    outputs:
+        manifest csv in same directory as script
+    '''
+    import glob
+    import pandas as pd
+    import re
+
+    directory = directory + "*.fastq.gz"
+    filenames = glob.glob(directory)
+
+    data = [] # empty list to fill, eventually make into DataFrame
+    sample_id = []
+    filepath =[]
+    direction = []
+
+    forward = "_R1_"
+    reverse = "_R2_"
+
+    for i in filenames: # sample ids
+        sample_id.append(re.search(expression,i).group())
+
+    for i in filenames: # read direction
+        if forward in i:
+            direction.append("forward")
+        elif reverse in i:
+            direction.append("reverse")
+
+    data = [sample_id,filenames,direction] # combining lists
+    df = pd.DataFrame(data).transpose() # need to transpose list of lists
+    df.columns = ["sample_id","filepath","direction"]
+    df.to_csv(output, index=False) #export
